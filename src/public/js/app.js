ff23232eb51b11c032eb657a6000e5c1175d593b
도영 황 <ghkdehdud96@naver.com>
2021-10-08 14:44:21 +0900

    2021-10-08

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-alert("hi");
+const frontSocket = new WebSocket(`ws://${window.location.host}`);
+
+frontSocket.addEventListener("open", () => {
+    console.log("서버와 연결되었습니다.✔");
+});
+
+frontSocket.addEventListener("message", (message) => {
+    console.log("새로운 메세지 : ",message.data);
+});
+
+frontSocket.addEventListener("close", ()  => {
+    console.log("서버와 연결이 해제되었습니다.❌");
+});
+
+setTimeout( () => {
+    frontSocket.send("안녕!");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
+import http from "http";
+import WebSocket from "ws";
 import express from"express";
+import { Socket } from 'dgram';
 
 const app = express();
 
@@ -6,6 +9,23 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+const handleListen = () => console.log(`http://localhost:3000 연결`);
+ 
+const server = http.createServer(app);
+const wss = new WebSocket.Server( {server} ); //websocket 서버 생성
+
+wss.on("connection", (backSocket) => {
+    console.log("브라우저와 연결되었습니다.✔");
+
+    backSocket.on("close",() => console.log("브라우저와 연결이 해제되었습니다.❌"))
+   
+    backSocket.on("message", (messege)=> {
+        console.log(messege.toString('utf-8'));
+    });
+    backSocket.send("Hello!");
+    //console.log(backSocket);
+});
+
+server.listen(3000, handleListen);
